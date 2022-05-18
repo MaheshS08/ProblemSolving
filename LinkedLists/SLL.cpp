3daa23068af756027fb0f3db420d797e8c8a4436
MaheshS08 <32458644+MaheshS08@users.noreply.github.com>
2022-05-18 15:25:40 +0530

    Create SLL.cpp

---
+++ LinkedLists/SLL.cpp
@@ -0,0 +1,79 @@
+#include <iostream>
+
+using namespace std;
+struct Node
+{
+    int data;
+    Node *next;
+    Node(int data)
+    {
+        this->data = data;
+    }
+} *head = NULL, *tail = NULL;
+
+void insertAtBeg()
+{
+    int data = 0;
+    cin >> data;
+    Node *newNode = new Node(data);
+    if (head == NULL)
+    {
+        head = newNode;
+        tail = newNode;
+    }
+    else
+    {
+        newNode->next = head;
+        head = newNode;
+    }
+}
+
+void insertAtEnd()
+{
+    int data = 0;
+    cin >> data;
+    Node *newNode = new Node(data);
+    if (head == NULL)
+    {
+        head = newNode;
+        tail = newNode;
+    }
+    else
+    {
+        tail->next = newNode;
+        tail = newNode;
+    }
+}
+
+void printList(Node *head)
+{
+    Node *temp = head;
+    if (head == NULL)
+    {
+        cout << "NULL";
+    }
+    while (temp != NULL)
+    {
+        cout << temp->data << endl;
+        temp = temp->next;
+    }
+}
+
+void printReverseList(Node *head)
+{
+    Node *temp = head;
+    if (temp->next == NULL)
+    {
+        cout << "NULL";
+    }
+    printReverseList(temp->next);
+    cout << temp->data << "->";
+}
+
+int main()
+{
+    insertAtBeg();
+    insertAtBeg();
+    insertAtBeg();
+    printList(head);
+}
